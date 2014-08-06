9d15a0f146f8a898b79a3023051e987c3f3bedfa
tonyskapunk <tonysk8@gmx.net>
2014-08-06 14:21:52 +0000

    Fixing validations and printing when no ts used.

--- linux/dmesg_log.py
+++ linux/dmesg_log.py
@@ -21,16 +21,21 @@ DMESG_OUTPUT = OUT.split('\n')
 with open(UPTIME, 'r') as f:
     UPTIME = f.read()
     NOW = datetime.now()
-f.closed()
+f.close()
 
 SECS_SINCE_BOOT = float(UPTIME.split(" ")[0])
 BOOT_TIME = NOW - timedelta(seconds=SECS_SINCE_BOOT)
 print "Booted: %s\n" % BOOT_TIME
 
 for line in DMESG_OUTPUT:
-    matched = re.match(r'^[\s*(\d*\.\d*)].*', line)
+    matched = re.match(r'^\s*[\s*(\d*\.\d*)].*', line)
     if matched:
-        ts_sec = matched.groups(1)[0]
-        td = timedelta(seconds=float(ts_sec))
-        ts = BOOT_TIME + td
-        print "[%s] - %s" % (ts, line)
+        if len(matched.groups(1)):
+            ts_sec = matched.groups(1)[0]
+            td = timedelta(seconds=float(ts_sec))
+            ts = BOOT_TIME + td
+            print "[%s] - %s" % (ts, line)
+        else:
+            print line
+    else:
+        print line        
